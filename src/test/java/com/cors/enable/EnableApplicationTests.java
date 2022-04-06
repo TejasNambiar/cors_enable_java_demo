a497138b714a582abb50728712b066908c8e25a1
Tejas Nambiar <tejasnambiar32@gmail.com>
2022-04-06 16:57:32 +0530

    cors enable java

---
+++ src/test/java/com/cors/enable/EnableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cors.enable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
